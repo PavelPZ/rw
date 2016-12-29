e4a9210e3b7a4338003fa978ee5062e197da8a2e
PZ <pzika@langmaster.cz>
2016-12-29 14:44:18 +0100

    v

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,10 @@ const imageDataURI = require('gulp-image-data-uri');
 const argv = require('yargs').argv;
 const rename = require("gulp-rename");
 const sizeOf = require('image-size');
-const debug = require('gulp-debug');
 const gulpIgnore = require('gulp-ignore');
+const clean = require('gulp-clean');
 
+//************** atomizer
 gulp.task('atomizer', () => {
   console.log('start');
   return gulp.src(['./rw-gui-rt/**/*.tsx', './rw-gui-rt/**/*.ts', './index.html'])
@@ -23,20 +24,23 @@ gulp.task('atomizer', () => {
     .pipe(gulp.dest('rw-gui-rt')); // the dir of output files
 });
 
-gulp.task('image-url', () => {
-  //console.log(argv);
-  let size = { width: -1, height: -1 };
-  return gulp.src([argv.path + '**/*.png', argv.path + '**/*.jpg', argv.path + '**/*.gif', argv.path + '**/*.bmp'])
-    .pipe(gulpIgnore.include(fn => {
-      Object.assign(size, sizeOf(fn.path));
-      return true;
-    }))
-    .pipe(imageDataURI({
-      template: {
-        file: './image-url-template.templ',
-        variables: size
-      }
-    }))
-    .pipe(rename(path => { path.basename = path.basename + path.extname; path.extname = '.ts'; }))
-    .pipe(gulp.dest(file => file.base)); // the dir of output files
+//************** image-url
+gulp.task('image-url', ['delete-image-url', 'create-image-url']);
+
+gulp.task('create-image-url', () => {
+  const path = argv.basicPath + argv.path;
+  let size = { width: -1, height: -1, type: '', origPath: '', id:'' };
+  return gulp.src([path + '**/*.png', path + '**/*.jpg', path + '**/*.gif', path + '**/*.bmp'])
+    .pipe(gulpIgnore.include(fn => { console.log('CREATE: ' + fn.path + '.ts'), Object.assign(size, sizeOf(fn.path)); size.origPath = fn.path.replace(/\\/g, '/'); size.id = size.origPath.substr(argv.basicPath.length - 1); return true; })) //side efect: assign size to global variable
+    .pipe(imageDataURI({ template: { file: './image-url-template.templ', variables: size } })) //generate .TS file by means of template and size
+    .pipe(rename(pth => { pth.basename = pth.basename + '.' + size.type; pth.extname = '.ts'; })) //rename new file
+    .pipe(gulp.dest(file => file.base)); // the dir of output files 
+});
+
+gulp.task('delete-image-url', () => {
+  const path = argv.basicPath + argv.path;
+  let size = { width: -1, height: -1, type: '', origPath: '', id: '' };
+  return gulp.src([path + '**/*.png.ts', path + '**/*.jpg.ts', path + '**/*.gif.ts', path + '**/*.bmp.ts'])
+    .pipe(gulpIgnore.include(fn => console.log('DELETE: ' + fn.path)))
+    .pipe(clean());
 });
