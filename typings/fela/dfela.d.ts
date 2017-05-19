9fdc83b46a467d46baaefc8c8f99d9f09e57eb8f
Pavel Zika <pzika@langmaster.cz>
2017-05-19 19:03:47 +0200

    v

--- typings/fela/dfela.d.ts
+++ typings/fela/dfela.d.ts
@@ -7,23 +7,6 @@
   type TPlugin = (style: IStyle) => IStyle; //http://fela.js.org/docs/advanced/Plugins.html
   type TEnhancer = (renderer: IRenderer) => IRenderer; //http://fela.js.org/docs/advanced/Enhancers.html
 
-  const enum TSubscribeMessageType {
-    rule = 1,
-    staticObject = 1,
-    keyframes = 2,
-    fontFace = 3,
-    staticString = 4,
-    clear = 5
-  }
-  interface ISubscribeMessage {
-    type: TSubscribeMessageType;
-  }
-  interface ISubscribeRuleOrStaticObjectMessage extends ISubscribeMessage { static?: boolean; declaration: string; selector: string; media: string; }
-  interface ISubscribeKeyframesMessage extends ISubscribeMessage { name: string; keyframe: string; }
-  interface ISubscribeFontFaceMessage extends ISubscribeMessage { fontFamily: string; fontFace: string; }
-  interface ISubscribeStaticStringMessage extends ISubscribeMessage { css: string; }
-  interface ISubscribeClearMessage extends ISubscribeMessage { }
-
   interface IRenderer {
     renderRule(rule: TRule, props: TRuleProps): string;
     renderKeyframe(keyFrame: TKeyFrame, props: TRuleProps): string;
@@ -33,6 +16,14 @@
     subscribe(event: (msg: ISubscribeRuleOrStaticObjectMessage | ISubscribeKeyframesMessage | ISubscribeFontFaceMessage | ISubscribeStaticStringMessage | ISubscribeClearMessage) => void): { unsubscribe: () => void; }
     clear();
   }
+
+  const enum TSubscribeMessageType { rule = 1, staticObject = 1, keyframes = 2, fontFace = 3, staticString = 4, clear = 5 }
+  interface ISubscribeMessage { type: TSubscribeMessageType; }
+  interface ISubscribeRuleOrStaticObjectMessage extends ISubscribeMessage { static?: boolean; declaration: string; selector: string; media: string; }
+  interface ISubscribeKeyframesMessage extends ISubscribeMessage { name: string; keyframe: string; }
+  interface ISubscribeFontFaceMessage extends ISubscribeMessage { fontFamily: string; fontFace: string; }
+  interface ISubscribeStaticStringMessage extends ISubscribeMessage { css: string; }
+  interface ISubscribeClearMessage extends ISubscribeMessage { }
 
   //http://fela.js.org/docs/advanced/RendererConfiguration.html
   interface IConfig {
